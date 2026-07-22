4f856f3b9e6b1dc2cc3b97d1ce3f3ed553a7c54b
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2026-07-22 10:57:29 +0200

    Remove unused turn result

--- lib/game/domain/turn.dart
+++ lib/game/domain/turn.dart
@@ -4,5 +4,4 @@ export 'turn/phases/advance_turn_phase.dart';
 export 'turn/phases/selection_refresh_phase.dart';
 export 'turn/turn_context.dart';
 export 'turn/turn_phase.dart';
-export 'turn/turn_result.dart';
 export 'turn/unit_turn_action_rules.dart';
